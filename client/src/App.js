649e7aaeecf2438418f139c572ec0b41b64a71b9
DiegoCuriel <a01640372@itesm.mx>
2023-04-27 21:50:13 -0600

    Update cases - Put added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import React from "react";
 import Component from './Component';
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <Menu />
           <Component />      
         </div>
       </header>
---
+++ client/src/Menu.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+
+export default function Menu() {
+  const [id, setId] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [precio, setPrecio] = useState('');
+  const [probabilidad, setProbabilidad] = useState('');
+  const [skins_disponibles, setSkinsDisponibles] = useState('');
+  const [fecha_lanzamiento, setFechaLanzamiento] = useState('');
+  const [rareza, setRareza] = useState('');
+  const [imagen_url, setImagenUrl] = useState('');
+
+  const handleUpdateData = async () => {
+    try {
+      const response = await fetch(`/updateCase/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          nombre,
+          descripcion,
+          precio,
+          probabilidad,
+          skins_disponibles,
+          fecha_lanzamiento,
+          rareza,
+          imagen_url
+        })
+      });
+      const data = await response.json();
+      console.log(data);
+      // Hacer algo con la respuesta de la API
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <p>Menu: </p>
+      <div>
+        <p>Id de la caja a modificar:</p>
+        <input value={id} onChange={(e) => setId(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Nombre:</p>
+        <input value={nombre} onChange={(e) => setNombre(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Descripción:</p>
+        <input value={descripcion} onChange={(e) => setDescripcion(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Precio:</p>
+        <input value={precio} onChange={(e) => setPrecio(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Probabilidad:</p>
+        <input value={probabilidad} onChange={(e) => setProbabilidad(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Skins Disponibles:</p>
+        <input value={skins_disponibles} onChange={(e) => setSkinsDisponibles(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Fecha de Lanzamiento:</p>
+        <input value={fecha_lanzamiento} onChange={(e) => setFechaLanzamiento(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Rareza:</p>
+        <input value={rareza} onChange={(e) => setRareza(e.target.value)}></input>
+      </div>
+      <div>
+        <p>Imagen URL:</p>
+        <input value={imagen_url} onChange={(e) => setImagenUrl(e.target.value)}></input>
+      </div>
+      <div>
+        <button onClick={handleUpdateData}>Actualizar Datos</button>
+      </div>
+    </div>
+  );
+}
